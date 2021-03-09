d719f094d9012044ed438f46a1df720ff8f17629
Khuong <lekhuongy@gmail.com>
2021-03-09 16:55:55 -0600

    updated server name

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? 'http://localhost:3000' : 'https://deployment.server.com'
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://spacenext.netlify.com";
