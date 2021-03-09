c3944ed0b2dce58ba40d392ee068cf10d63eb335
Khuong <lekhuongy@gmail.com>
2021-03-09 16:06:30 -0600

    commented out exp code

--- pages/services/getId.tsx
+++ pages/services/getId.tsx
@@ -1,8 +1,10 @@
-export const getId = async () => {
-  const req = await fetch(`${server}/api/rockets`)
-  const rocketData : RocketData[] = await req.json();
-  const ids = rocketData.map(rocket => rocket.id)
-  const paths = ids.map(id => ({params: {rocket: id}}))
 
-  return paths
-}
+
+// export const getId = async () => {
+//   const req = await fetch(`${server}/api/rockets`)
+//   const rocketData : RocketData[] = await req.json();
+//   const ids = rocketData.map(rocket => rocket.id)
+//   const paths = ids.map(id => ({params: {rocket: id}}))
+
+//   return paths
+// }
