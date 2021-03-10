b230982af201ac660faa8d1273e0f0be1795781c
Khuong <lekhuongy@gmail.com>
2021-03-09 18:28:23 -0600

    added agent-user

--- pages/api/rockets.tsx
+++ pages/api/rockets.tsx
@@ -3,10 +3,18 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (_: NextApiRequest, res: NextApiResponse) => {
   try {
-    const req = await fetch("https://api.spacexdata.com/v4/rockets");
+    const req = await fetch("https://api.spacexdata.com/v4/rockets", {
+      method: "GET",
+      headers: {
+        // update with your user-agent
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+        Accept: "application/json; charset=UTF-8",
+      },
+    });
     const rocketData: RocketData[] = await req.json();
     res.json(rocketData);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
