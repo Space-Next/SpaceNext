cf30ee06ca2d85a95f89b3ae5c719043877e521b
Asharon Baltazar <asharonbaltazar@outlook.com>
2021-03-10 22:35:24 -0600

    Added spinner to rocket page

--- pages/rockets/index.tsx
+++ pages/rockets/index.tsx
@@ -1,30 +1,31 @@
 import { useEffect } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { GetStaticProps } from "next";
 import { FormattedRocketData } from "../../types";
-import {GetIds} from "../../services/Rockets"
+import { GetIds } from "../../services/Rockets";
+import { SpiralSpinner } from "react-spinners-kit";
 interface IProps {
   formattedData: FormattedRocketData[];
 }
 
 const Rockets = ({ formattedData }: IProps) => {
-  const id  = formattedData[0];
-  const router = useRouter()
+  const id = formattedData[0];
+  const router = useRouter();
 
-  useEffect (() => {
-    router.push(`rockets/${id}`)
-  })
+  useEffect(() => {
+    router.push(`rockets/${id}`);
+  });
 
   return (
-    <div className="text-white">
-      Loading
+    <div className="w-full mt-10 flex items-center justify-center">
+      <SpiralSpinner frontColor={"rgb(245, 158, 11)"} size={150} />
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const formattedData = await GetIds()
-  
+  const formattedData = await GetIds();
+
   return {
     props: {
       formattedData,
