a192e729e636f90823ff13fbd73ae01d4b9a79ec
Khuong <lekhuongy@gmail.com>
2021-03-10 21:35:11 -0600

    fix styling bugs in build

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,14 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+    ? {
+         '@fullhuman/postcss-purgecss': {
+            content: ['./components/**/*.jsx', './pages/**/*.js'],
+            defaultExtractor: content =>
+               content.match(/[\w-/:]+(?<!:)/g) || [],
+         },
+      }
+    : {}),
   },
 }
